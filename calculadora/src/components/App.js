bef98b191787ac9e6b4e92983a47e87b15d08885
Raphael Nascimento <raphael.nsc.job@gmail.com>
2020-09-06 14:42:18 -0300

    Preparação da estrutura e estilização

---
+++ calculadora/src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Calculadora from './Calculadora';
+
+export default class App extends Component{
+  render(){
+    return(
+      <>
+      <Calculadora />
+      </>
+    );
+  }
+}
---
+++ calculadora/src/components/Calculadora.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../styles/calculadora.css';
+
+export default class Calculadora extends Component{
+  render(){
+    return(
+      <>
+      <h1>Calc</h1>
+      <div className="calculadora"></div>
+      </>
+    );
+  }
+}
